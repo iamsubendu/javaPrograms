5ffeaf57bcc0fdf86c6f2471754f19258e82bb7a
iamsubendu <66836529+iamsubendu@users.noreply.github.com>
2023-09-18 17:36:35 +0530

    added first 2 programs of java

---
+++ Second.java
@@ -0,0 +1,15 @@
+public class Second {
+    // Create a Java Program, declare variables of type
+    // int, float, char, boolean, String, double
+    // Initialize all the variable with values and display them on the console.
+
+    public static void main(String[] args) {
+        int a = 9;
+        float b = 9;
+        // Java interprets "C" as a string literal, not a character literal.
+        char c = 'C';
+        boolean d = true;
+        double e = 9;
+        System.out.println("a: " + a + ", b: " + b + ", c: " + c + ", d: " + d + ", e: " + e);
+    }
+}
